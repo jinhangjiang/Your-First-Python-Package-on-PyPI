c8b04a2a1cff4406593685665c074276fad5896b
Jinhang Jiang <60441304+jinhangjiang@users.noreply.github.com>
2022-05-14 16:19:03 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,1 @@
-# leave it empty. This is just a special file that tells pip that your main module is in this folder. No need to add anything here.
+# leave it empty. This is just a special file that tells pip that your main module is in this folder. No need to add anything here. Feel free to delete this line when you make your own package.
